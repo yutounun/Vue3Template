b92af8ba9f76a8bfb21dd5b18b8b02910b153942
yutounun <geek.yuto@gmail.com>
2023-01-28 18:02:33 +0900

    Comment out firehd information

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -8,15 +8,17 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCPXCKWHLMhIBA9e_IXFxpBuFVth2_Qvo0",
-  authDomain: "workout-app-5e81f.firebaseapp.com",
-  projectId: "workout-app-5e81f",
-  storageBucket: "workout-app-5e81f.appspot.com",
-  messagingSenderId: "46791203415",
-  appId: "1:46791203415:web:e884810893899ea3dbe112",
-  measurementId: "G-31QF607P15",
-};
+
+// Copy and paste the config code as below
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCPXCKWHLMhIBA9e_IXFxpBuFVth2_Qvo0",
+//   authDomain: "workout-app-5e81f.firebaseapp.com",
+//   projectId: "workout-app-5e81f",
+//   storageBucket: "workout-app-5e81f.appspot.com",
+//   messagingSenderId: "46791203415",
+//   appId: "1:46791203415:web:e884810893899ea3dbe112",
+//   measurementId: "G-31QF607P15",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
